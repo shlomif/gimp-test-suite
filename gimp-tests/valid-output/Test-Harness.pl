6f4ea973edd078c2b9397d3a8082d9e42aaac0e6
shlomif <shlomif@b2b4ac75-4df0-0310-84b2-a54a85d66599>
2005-02-21 11:50:06 +0000

    Gimp-Test: Valid-Output:
    Now performing the tests in Test-Harness.pl.

--- gimp-tests/valid-output/Test-Harness.pl
+++ gimp-tests/valid-output/Test-Harness.pl
@@ -5,6 +5,8 @@ use warnings;
 
 use Net::SeedServe::Server;
 use String::ShellQuote;
+use Time::HiRes qw(usleep);
+use IO::All;
 
 use constant GIMP_VER => "2.3";
 
@@ -24,7 +26,7 @@ sub find_grand_seedserve_lib
 }
 
 my $seed_server_status_file = "temp/server-status.txt";
-# First of all - start the service.
+# First of all - start the seed service.
 my $seed_server =
     Net::SeedServe::Server->new(
         'status_file' => $seed_server_status_file,
@@ -34,6 +36,9 @@ my $seed_server_port = $seed_server->start()->{'port'};
 
 my $grand_ss_path = find_grand_seedserve_lib();
 
+my $gimp_perl_server_socket_path = `perl get-socket-path.pl`;
+
+unlink($gimp_perl_server_socket_path);
 # Start the gimp
 {
     local $ENV{'SEEDSERVE_PORT'} = $seed_server_port;
@@ -44,8 +49,40 @@ my $grand_ss_path = find_grand_seedserve_lib();
         );
 }
 
+# Wait for the Perl server to run.
+while (! -e $gimp_perl_server_socket_path)
+{
+    usleep(5000);
+}
+usleep(5000);
+
+my (@failed, @passed);
+# Now run the tests
+foreach my $file (io("./gen-scripts/")->all_files())
+{
+    my $filename = $file->filename();
+    next if ($filename =~ /~$/);
+    my $test_name = $filename;
+    $test_name =~ s/\.pl$//;
+    print STDERR "Performing Test \"$test_name\"\n";
+    my $ret = system("perl", "$file", "--mode=check", "--name=$test_name");
+    if ($ret)
+    {
+        push @failed, $test_name;
+    }
+    else
+    {
+        push @passed, $test_name;
+    }
+}
+
 print STDERR "Type <Return> to exit.\n";
 my $line = <STDIN>;
+
+io("failed-report.txt")->print(map { "$_\n" } ("Failed", "", @failed));
+print STDERR "Passed " . scalar(@passed) .
+    "; Failed " . scalar(@failed) . "\n";
+print STDERR "Report of failed tests written to failed-report.txt.\n";
 
 END
 {
---
+++ gimp-tests/valid-output/get-socket-path.pl
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+
+use Gimp::Net;
+
+print $Gimp::Net::default_unix_dir.$Gimp::Net::default_unix_sock;
+
