80d296f8cef11dfc3f1fb12135febbeb2f0c40b1
shlomif <shlomif@b2b4ac75-4df0-0310-84b2-a54a85d66599>
2005-02-21 09:29:05 +0000

    Gimp-Test: Valid-Output:
    Created a module (GimpTest::Run) to encapsulate the common testing
    functionality.

---
+++ gimp-tests/valid-output/GimpTest/Run.pm
@@ -0,0 +1,96 @@
+use strict;
+use warnings;
+
+use Gimp ":auto";
+
+use Cwd;
+use Digest::MD5;
+use YAML (qw(DumpFile LoadFile));
+use Getopt::Long;
+
+my $mode = undef;
+my $test_name = undef;
+my %options = ();
+
+my $cmd_line_ok = 
+    GetOptions(
+        "mode=s" => \$mode,
+        "name=s" => \$test_name,
+        "options=s" => \%options,
+    );
+
+if (!$cmd_line_ok)
+{
+    die "Incorrect options passed.";
+}
+if (!defined($mode))
+{
+    die "Mode not specified.";
+}
+if (!(($mode eq "init") || ($mode eq "check")))
+{
+    die "Unknown mode \"$mode\".";
+}
+if (!defined($test_name))
+{
+    die "Test name not specified.";
+}
+
+Gimp::init();
+
+sub get_drawable
+{
+    my $img = shift;
+    return (gimp_image_get_layers($img))[0];
+}
+
+sub load_input_file
+{
+    my $filename = shift;
+    my $input_fn = getcwd() . "/input-images/$filename";
+    return gimp_file_load(1, ($input_fn) x 2);
+}
+
+my $output_fn = getcwd() . "/temp/output-images/$test_name.bmp";
+my $img = main::gen_image()->{'image_id'};
+gimp_image_flatten($img);
+gimp_file_save(1, $img, get_drawable($img), $output_fn, $output_fn);
+gimp_image_delete($img);
+
+my $signature = +{};
+$signature->{'length'} = (-s $output_fn);
+$signature->{'hash_functions'} = ["md5"];
+{
+    local *I;
+    open I, "<", $output_fn;
+    my $md5 = Digest::MD5->new();
+    $signature->{'hash_values'} = +{ 'md5' => $md5->addfile(*I)->hexdigest() };
+    close(I);
+}
+my $sign_fn = "output-sigs/$test_name.yml";
+if ($mode eq "init")
+{
+    DumpFile($sign_fn, $signature);
+}
+elsif ($mode eq "check")
+{
+    my ($expected_sign) = LoadFile($sign_fn);
+    if ($expected_sign->{'length'} != $signature->{'length'})
+    {
+        die "Length Mismatch!";
+    }
+    foreach my $func (@{$signature->{'hash_functions'}})
+    {
+        if ($expected_sign->{'hash_values'}->{$func} ne
+            $signature->{'hash_values'}->{$func})
+        {
+            die "Hash Value Mismatch for \"$func\"!";
+        }
+    }
+}
+print "Success!\n";
+
+unlink($output_fn);
+
+1;
+
--- gimp-tests/valid-output/gen-scripts/gauss_blur_rle.pl
+++ gimp-tests/valid-output/gen-scripts/gauss_blur_rle.pl
@@ -3,70 +3,16 @@
 use strict;
 use warnings;
 
-use Gimp ":auto";
-
-use Cwd;
-use Digest::MD5;
-use YAML (qw(DumpFile LoadFile));
-
-Gimp::init();
-
-sub get_drawable
-{
-    my $img = shift;
-    return (gimp_image_get_layers($img))[0];
-}
+# This is done to execute the test wrapper.
+require GimpTest::Run;
 
 # This function will be modified from script to script.
 sub gen_image
 {
-    my $input_fn = getcwd() . "/input-images/tiger_sitting.png";
-    my $img = gimp_file_load(1, $input_fn, $input_fn);
+    my $img = load_input_file("tiger_sitting.png");
 
     plug_in_gauss(1, $img, get_drawable($img), 5.0, 10.0, 1);
 
     return { 'image_id' => $img };
 }
-
-my $output_fn = getcwd() . "/temp/output-images/gauss_blur_rle.bmp";
-my $img = gen_image()->{'image_id'};
-gimp_image_flatten($img);
-gimp_file_save(1, $img, get_drawable($img), $output_fn, $output_fn);
-gimp_image_delete($img);
-
-my $do_what = shift(@ARGV);
-my $signature = +{};
-$signature->{'length'} = (-s $output_fn);
-$signature->{'hash_functions'} = ["md5"];
-{
-    local *I;
-    open I, "<", $output_fn;
-    my $md5 = Digest::MD5->new();
-    $signature->{'hash_values'} = +{ 'md5' => $md5->addfile(*I)->hexdigest() };
-    close(I);
-}
-my $sign_fn = "output-sigs/gauss_blur_rle.yml";
-if ($do_what eq "init")
-{
-    DumpFile($sign_fn, $signature);
-}
-elsif ($do_what eq "check")
-{
-    my ($expected_sign) = LoadFile($sign_fn);
-    if ($expected_sign->{'length'} != $signature->{'length'})
-    {
-        die "Length Mismatch!";
-    }
-    foreach my $func (@{$signature->{'hash_functions'}})
-    {
-        if ($expected_sign->{'hash_values'}->{$func} ne
-            $signature->{'hash_values'}->{$func})
-        {
-            die "Hash Value Mismatch for \"$func\"!";
-        }
-    }
-}
-print "Success!\n";
-
-unlink($output_fn);
 
