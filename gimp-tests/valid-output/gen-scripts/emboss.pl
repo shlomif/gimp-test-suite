1ae3fa84c1ea948ac8a79c5cf554a463cba1fe06
shlomif <shlomif@b2b4ac75-4df0-0310-84b2-a54a85d66599>
2005-02-24 18:20:51 +0000

    Added the Emboss test.

---
+++ gimp-tests/valid-output/gen-scripts/emboss.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+# This is done to execute the test wrapper.
+require GimpTest::Run;
+
+# This function will be modified from script to script.
+sub gen_image
+{
+    my $img = load_input_file("tiger_sitting.png");
+
+    gimp_rect_select($img, 20, 30, 150, 100, CHANNEL_OP_REPLACE(), 0, 0);
+
+    plug_in_emboss(1, $img, get_layer($img), 30, 100, 40, 1);
+
+    return { 'image_id' => $img };
+}
+
