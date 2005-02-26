625e7704d8ead7ff89520c8527d82585df3c5f55
shlomif <shlomif@b2b4ac75-4df0-0310-84b2-a54a85d66599>
2005-02-26 09:33:29 +0000

    Valid-Output:
    Added a good high-resolution input image along with a script to make
    sure it is loaded the same way everytime.

---
+++ gimp-tests/valid-output/gen-scripts/shlomif-sitting-meta.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl -w
+
+# This is a meta script that checks that the uncompressed contents of
+# a JPEG file are the same any-time it is loaded.
+#
+use strict;
+use warnings;
+
+# This is done to execute the test wrapper.
+require GimpTest::Run;
+
+# This function will be modified from script to script.
+sub gen_image
+{
+    my $img = load_input_file("shlomif-sitting.jpg");
+
+    return { 'image_id' => $img };
+}
+
