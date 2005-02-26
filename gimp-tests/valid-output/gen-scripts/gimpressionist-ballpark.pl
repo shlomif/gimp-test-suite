9c5a680122f10d0dca9dee94b57b051e7a39e7f0
shlomif <shlomif@b2b4ac75-4df0-0310-84b2-a54a85d66599>
2005-02-26 11:07:49 +0000

    Valid-Output:
    Added the gimpressionist-ballpark test.

---
+++ gimp-tests/valid-output/gen-scripts/gimpressionist-ballpark.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+# This is done to execute the test wrapper.
+require GimpTest::Run;
+
+# This function will be modified from script to script.
+sub gen_image
+{
+    init_seeds([50000]);
+    my $img = load_input_file("shlomif-sitting.jpg");
+
+    plug_in_gimpressionist(RUN_NONINTERACTIVE(), 
+        $img, get_layer($img), "Ballpark"
+        );
+
+    return { 'image_id' => $img };
+}
+
